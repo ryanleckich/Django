cecae5e023c37fca2d4842290e3ec4dc2b012cd8
ryanleckich <leckich9@gmail.com>
2022-05-05 23:04:37 -0500

    part 1-1

--- FeedApp/urls.py
+++ FeedApp/urls.py
@@ -6,5 +6,5 @@ app_name = "FeedApp"
 urlpatterns = [
     path("", views.index, name="index"),
     path("profile/", views.profile, name="profile"),
-    # path("myfeed", views.myfeed, name="myfeed"),
+    path("myfeed", views.myfeed, name="myfeed"),
 ]
--- FeedApp/views.py
+++ FeedApp/views.py
@@ -42,11 +42,13 @@ def myfeed(request):
     comment_count_list = []
     like_count_list = []
     posts = Post.objects.filter(username=request.user).order_by("-date_posted")
+    # grab all the posts that belong to a certain user # descending order by date
     for p in posts:
-        c_count = Comment.objects.filter(post=p).count()
+        c_count = Comment.objects.filter(post=p).count()  # number of comments
         l_count = Like.objects.filter(post=p).count()
-        comment_count_list.append(l_count)
+        comment_count_list.append(c_count)
         like_count_list.append(l_count)
+
     zipped_list = zip(posts, comment_count_list, like_count_list)
 
     context = {"posts": posts, "zipped_list": zipped_list}
